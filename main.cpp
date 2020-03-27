5b55dfb4a981bfc3f982033595c914e87a0b1d43
Lachlan Lancaster <lachlanl@princeton.edu>
2020-03-27 13:14:31 -0400

    added a gitignore and code toread in the IMF file

--- main.cpp
+++ main.cpp
@@ -14,9 +14,10 @@
 int main()
 {
 	/* definition of global parameters, probably will 
-	 * be read in from stdin in later development */
+	 * be read in from stdin in later development 
+	 */
 
-	// surface birghtness in mags/ arcsec^2
+	// surface birghtness in mags/arcsec^2
 	Doub S = 33.; 
 	// exposure time in seconds
 	Doub t_expose = 1000.;
@@ -24,6 +25,32 @@ int main()
 	Doub d = 1.;
 	// distance modulus 
 	Doub mu = 5.*log10(d*1e5);
+
+	/* Read in initializing files such as the IMF 
+	 * and the isochrones 
+	 */
+	// maximum number of initial masses being read in
+	Int maxN_IMF = 6000000;
+	Doub temp;
+	Doub *kmass;
+	kmass = (Doub*) malloc(sizeof(Doub)*maxN_IMF);
+
+	// reading in IMF file
+	ifstream massfile ("mid_set.txt");
+	if(!massfile.is_open()){
+		cerr << "There was a problem opening the IMF file \n";
+		return 1;
+	}
+	Int i = 0;
+	while (massfile>>temp) {
+		kmass[i] = temp;
+		i += 1;
+	}
+	massfile.close();
+
+	// 
+
+
 	/* code */
 	return 0;
 }
