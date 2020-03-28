30ebf8f762e4cba36ef0b893a29fc7bd38194dec
Lachlan Lancaster <lachlanl@princeton.edu>
2020-03-28 12:13:02 -0400

    testing the shuffling

--- main.cpp
+++ main.cpp
@@ -155,12 +155,16 @@ int main()
 
 	// allocate a index array that will be repeatedley shuffled
 	// in order to Monte-Carlo sample the IMF
-	array<int,4> index_arr = {0,1,2,3};
+	array<int,10> index_arr = {0,1,2,3,4,5,6,7,8,9};
 	/*for (i=0;i<4;i++){
 		index_arr[i] = i;
 		}*/
 	unsigned seed = 42;
 	shuffle (index_arr.begin(), index_arr.end(), default_random_engine(seed));
+
+	cout << "Shuffled elements: ";
+	for(int &x index_arr) cout << ' ' << x;
+	cout << "\n";
 
 	// de-allocate memory
 	free(kmass);
