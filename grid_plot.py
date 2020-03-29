8955673030566d3a4a45198d43c5a467399eb5a5
Lachlan Lancaster <lachlanl@princeton.edu>
2020-03-29 17:18:11 -0400

    added some additions to the plotting file

--- grid_plot.py
+++ grid_plot.py
@@ -7,8 +7,23 @@ import matplotlib.pyplot as plt
 
 
 if __name__ == '__main__':
-	arr = np.loadtxt("output.txt")
 
-	plt.imshow(np.log10(arr),extent=(28.,33.,5.,1.))
+	J_outs = np.loadtxt("outputs/J_out.txt")
+	J_detect_5sig = 25.939
+
+	arr = np.loadtxt("outputs/feh1_age9005.txt")
+	d = np.linspace(1.,25.,51)
+	mucorr = J_detect_5sig - J_outs
+	dcorr = 1e-5*(10**(mucorr/5))
+
+	darr = np.array([d for i in range(arr.shape[1])]).T
+
+	ax = plt.subplot(211)
+	plt.imshow(np.log10(arr),extent=(28.,33.,25.,1.))
 	plt.colorbar()
+	ax.set_aspect(0.2)
+	ax2 = plt.subplot(212)
+	plt.hist(dcorr,bins="auto",histtype="step")
+	plt.xlim(1.,25.)
+	plt.yscale("log")
 	plt.show()
