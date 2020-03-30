153d4c302f7dd27b50b01428bccdaca0c821b6e7
Lachlan Lancaster <lachlanl@princeton.edu>
2020-03-30 15:51:15 -0400

    reorganized the code so that it should be able to loop nicely over the IMFs

--- grid_plot.py
+++ grid_plot.py
@@ -11,7 +11,12 @@ if __name__ == '__main__':
 	J_outs = np.loadtxt("outputs/J_out.txt")
 	J_detect_5sig = 25.939
 
-	arr = np.loadtxt("outputs/feh1_age9005.txt")
+	arr1 = np.loadtxt("outputs/feh1_age9005_set1.txt")
+	arr2 = np.loadtxt("outputs/feh1_age9005_set2.txt")
+	arr3 = np.loadtxt("outputs/feh1_age9005_set3.txt")
+	arr4 = np.loadtxt("outputs/feh1_age9005_set4.txt")
+	arr5 = np.loadtxt("outputs/feh1_age9005_set5.txt")
+	arr = (arr1+arr2+arr3+arr4+arr5)/5.
 	d = np.linspace(1.,25.,51)
 	mucorr = J_detect_5sig - J_outs
 	dcorr = 1e-5*(10**(mucorr/5))
@@ -19,11 +24,11 @@ if __name__ == '__main__':
 	darr = np.array([d for i in range(arr.shape[1])]).T
 
 	ax = plt.subplot(211)
-	plt.imshow(np.log10(arr),extent=(28.,33.,25.,1.))
+	plt.imshow(np.log10(arr),extent=(28.,33.,20.,1.))
 	plt.colorbar()
 	ax.set_aspect(0.2)
 	ax2 = plt.subplot(212)
 	plt.hist(dcorr,bins="auto",histtype="step")
-	plt.xlim(1.,25.)
+	plt.xlim(1.,20.)
 	plt.yscale("log")
 	plt.show()
--- main.cpp
+++ main.cpp
@@ -40,9 +40,6 @@ int main()
 	char outname[100];
 	int len;
 	int n_set = 1;
-	len = sprintf(outname,"/home/lachlanl/WFIRST_resolved_count/outputs/feh1_age9005_set%d.txt",n_set);
-	cout << outname << "\n";
-	output.open(outname);
 
 	// defintion of field of view
 	// which is used as the FoV at 1 Mpc and 
@@ -63,6 +60,7 @@ int main()
 	// These are used for allocating memory, could cause segfaults maybe
 	// maximum number of initial masses being read in from IMF
 	int maxN_IMF = 3000000;
+	int N_IMF_sets = 1; // number of IMFs to loop over
 	// maximum number of lines to be read from isochrone file
 	int maxN_iso = 1600;
 
@@ -94,34 +92,9 @@ int main()
 	// AB zero-point flux definitin in ergs s^-1 Hz^-1
 	double f_ab_zero = 6.626e-27;
 
-
-	/* Read in initializing files such as the IMF 
-	 * and the isochrones 
+	/* Allocating memory for storage of IMF and isochrone values
 	 */
-	int N_IMF;
-	double temp;
-	double *kmass;
-	kmass = (double*) malloc(sizeof(double)*maxN_IMF);
-
-	// reading in IMF file
-	char mfilename[100];
-	len = sprintf(mfilename,"/home/lachlanl/WFIRST_resolved_count/imfs/set%d.txt",n_set);
-	ifstream massfile (mfilename);
-	if(!massfile.is_open()){
-		cerr << "There was a problem opening the IMF file \n";
-		return 1;
-	}
-	i = 0;
-	while (massfile>>temp) {
-		kmass[i] = temp;
-		i += 1;
-	}
-	N_IMF = i;
-	massfile.close();
-
-	/* Reading in the isochrone so that the IMF can be 
-	 * interpolated and turned in to colors/magnitudes
-	 */
+	// first for the isochrones
 	int N_iso;
 	double *imass, *Z, *Y, *J, *H, *F;
 	double *Z_out, *Y_out, *J_out, *H_out, *F_out;
@@ -136,7 +109,18 @@ int main()
 	J_out = (double*) malloc(sizeof(double)*N_IMF);
 	H_out = (double*) malloc(sizeof(double)*N_IMF);
 	F_out = (double*) malloc(sizeof(double)*N_IMF);
-
+	// then for the IMF
+	int N_IMF;
+	double temp;
+	double *kmass;
+	kmass = (double*) malloc(sizeof(double)*maxN_IMF);
+	// fluxes that will be used in sampling below
+	double *f_Z, *f_Y, *f_J, *f_H, *f_F;
+	f_Z = (double *) malloc(sizeof(double)*N_imf_filt);
+	f_Y = (double *) malloc(sizeof(double)*N_imf_filt);
+	f_J = (double *) malloc(sizeof(double)*N_imf_filt);
+	f_H = (double *) malloc(sizeof(double)*N_imf_filt);
+	f_F = (double *) malloc(sizeof(double)*N_imf_filt);
 	// Reading isochrone file
 	ifstream isofile ("/home/lachlanl/WFIRST_resolved_count/isos/iso_age9005.txt");
 	if(!isofile.is_open()){
@@ -166,101 +150,111 @@ int main()
 	N_iso = i;
 	isofile.close();
 
-	// interpolate on to the IMF and convert from
-	// Vega magnitudes to AB magnitudes
-	int N_imf_filt=0;
-	for(j=0;j<N_IMF;j++){
-		if ((kmass[j] < max_imass) && (kmass[j]>min_imass)){
-			Z_out[N_imf_filt] = linear_interp(kmass[j],imass,Z) + AB_Vega[0];
-			Y_out[N_imf_filt] = linear_interp(kmass[j],imass,Y) + AB_Vega[1];
-			J_out[N_imf_filt] = linear_interp(kmass[j],imass,J) + AB_Vega[2];
-			H_out[N_imf_filt] = linear_interp(kmass[j],imass,H) + AB_Vega[3];
-			F_out[N_imf_filt] = linear_interp(kmass[j],imass,F) + AB_Vega[4];
-			N_imf_filt++;
+	// we will loop over reading IMFS, so that we can sample many practice IMFs
+	int l;
+	for (l = 0;l<N_IMF_sets;l++){
+		// initialize where the output will be saved
+		len = sprintf(outname,"/home/lachlanl/WFIRST_resolved_count/outputs/feh1_age9005_set%d.txt",n_set);
+		cout << outname << "\n";
+		output.open(outname);
+		// reading in IMF file
+		char mfilename[100];
+		len = sprintf(mfilename,"/home/lachlanl/WFIRST_resolved_count/imfs/set%d.txt",l);
+		ifstream massfile (mfilename);
+		if(!massfile.is_open()){
+			cerr << "There was a problem opening the IMF file \n";
+			return 1;
 		}
-	}
+		i = 0;
+		while (massfile>>temp) {
+			kmass[i] = temp;
+			i += 1;
+		}
+		N_IMF = i;
+		massfile.close();
 
-	// allocate a index array that will be repeatedley shuffled
-	// in order to Monte-Carlo sample the IMF
-	//array<int,10> index_arr = {0,1,2,3,4,5,6,7,8,9};
-	/*for (i=0;i<4;i++){
-		index_arr[i] = i;
-		}*/
-	//unsigned seed = 42;
-	//shuffle (index_arr.begin(), index_arr.end(), default_random_engine(seed));
+		/* Reading in the isochrone so that the IMF can be 
+		 * interpolated and turned in to colors/magnitudes
+		 */
 
-	double *f_Z, *f_Y, *f_J, *f_H, *f_F;
-	f_Z = (double *) malloc(sizeof(double)*N_imf_filt);
-	f_Y = (double *) malloc(sizeof(double)*N_imf_filt);
-	f_J = (double *) malloc(sizeof(double)*N_imf_filt);
-	f_H = (double *) malloc(sizeof(double)*N_imf_filt);
-	f_F = (double *) malloc(sizeof(double)*N_imf_filt);
-
-
-	for(k=0;k<n_d;k++){
-		d = dmin + k*deld;
-		// distance modulus 
-		mu = 5.*log10(d*1e5);
-		cout << "For distance of " << d << " Mpc" << "\n"; 
-		for (j = 0;j<n_S;j++){
-			// set the current value of surface brightness to look at
-			S = Smin + j*dS;
-			//cout << "  For a surface brightness of " << S <<  "  mags/ as^2" <<"\n";
-			// set uppper flux limits
-			double S_J = S + AB_Vega[2] - ab_zeros[2];
-			double f_S_J = f_ab_zero*nu_c[2] * pow(10,((S_J)/(-2.5)));
-			double f_S_J_tot = f_S_J*(FoV_as/(d*d));
-			double S_H = S + AB_Vega[3] - ab_zeros[3];
-			double f_S_H = f_ab_zero*nu_c[3] * pow(10,((S_H)/(-2.5)));
-			double f_S_H_tot = f_S_H*(FoV_as/(d*d));
-			double S_F = S + AB_Vega[4] - ab_zeros[4];
-			double f_S_F = f_ab_zero*nu_c[4] * pow(10,((S_F)/(-2.5)));
-			double f_S_F_tot = f_S_F*(FoV_as/(d*d));
-
-			//cout << "    Flux limit in J band is " << f_S_J_tot << "ergs/s" << "\n";
-
-			//fluxes in photon counts per second
-			for (i=0;i<N_imf_filt;i++){
-				f_Z[i] = f_ab_zero*nu_c[0] * pow(10, (Z_out[i] - ab_zeros[0] + mu)/(-2.5));
-				f_Y[i] = f_ab_zero*nu_c[1] * pow(10, (Y_out[i] - ab_zeros[1] + mu)/(-2.5));
-				f_J[i] = f_ab_zero*nu_c[2] * pow(10, (J_out[i] - ab_zeros[2] + mu)/(-2.5));
-				f_H[i] = f_ab_zero*nu_c[3] * pow(10, (H_out[i] - ab_zeros[3] + mu)/(-2.5));
-				f_F[i] = f_ab_zero*nu_c[4] * pow(10, (F_out[i] - ab_zeros[4] + mu)/(-2.5));
+		// interpolate on to the IMF and convert from
+		// Vega magnitudes to AB magnitudes
+		int N_imf_filt=0;
+		for(j=0;j<N_IMF;j++){
+			if ((kmass[j] < max_imass) && (kmass[j]>min_imass)){
+				Z_out[N_imf_filt] = linear_interp(kmass[j],imass,Z) + AB_Vega[0];
+				Y_out[N_imf_filt] = linear_interp(kmass[j],imass,Y) + AB_Vega[1];
+				J_out[N_imf_filt] = linear_interp(kmass[j],imass,J) + AB_Vega[2];
+				H_out[N_imf_filt] = linear_interp(kmass[j],imass,H) + AB_Vega[3];
+				F_out[N_imf_filt] = linear_interp(kmass[j],imass,F) + AB_Vega[4];
+				N_imf_filt++;
 			}
+		}
 
+		for(k=0;k<n_d;k++){
+			d = dmin + k*deld;
+			// distance modulus 
+			mu = 5.*log10(d*1e5);
+			cout << "For distance of " << d << " Mpc" << "\n"; 
+			for (j = 0;j<n_S;j++){
+				// set the current value of surface brightness to look at
+				S = Smin + j*dS;
+				//cout << "  For a surface brightness of " << S <<  "  mags/ as^2" <<"\n";
+				// set uppper flux limits
+				double S_J = S + AB_Vega[2] - ab_zeros[2];
+				double f_S_J = f_ab_zero*nu_c[2] * pow(10,((S_J)/(-2.5)));
+				double f_S_J_tot = f_S_J*(FoV_as/(d*d));
+				double S_H = S + AB_Vega[3] - ab_zeros[3];
+				double f_S_H = f_ab_zero*nu_c[3] * pow(10,((S_H)/(-2.5)));
+				double f_S_H_tot = f_S_H*(FoV_as/(d*d));
+				double S_F = S + AB_Vega[4] - ab_zeros[4];
+				double f_S_F = f_ab_zero*nu_c[4] * pow(10,((S_F)/(-2.5)));
+				double f_S_F_tot = f_S_F*(FoV_as/(d*d));
 
-			// cumulative sum of flux array
-			double CS_fJ = 0;
-			int n_needed = 0;
-			while((CS_fJ < f_S_J_tot) && (n_needed < N_imf_filt)){
-				CS_fJ += f_J[n_needed];
-				n_needed++;
-			}
-			// check to make cure that the whole filter wasn't needed
-			if (n_needed==N_imf_filt){
-				cout << "Used the entire IMF. You should decrease the size of the field or increase the IMF sample." << "\n";
-			}
-			else if (n_needed<100){
-				cout << "Needed less than 100 stars to reach required luminosity.\n";
-			}
+				//cout << "    Flux limit in J band is " << f_S_J_tot << "ergs/s" << "\n";
 
-			// count the number that are above the detection threshold
-			int n_detected = 0;
-			for(i=0;i<n_needed;i++){
-				if(J_out[i] + mu<ps_detect_5slim[2]){
-					n_detected++;
+				//fluxes in photon counts per second
+				for (i=0;i<N_imf_filt;i++){
+					f_Z[i] = f_ab_zero*nu_c[0] * pow(10, (Z_out[i] - ab_zeros[0] + mu)/(-2.5));
+					f_Y[i] = f_ab_zero*nu_c[1] * pow(10, (Y_out[i] - ab_zeros[1] + mu)/(-2.5));
+					f_J[i] = f_ab_zero*nu_c[2] * pow(10, (J_out[i] - ab_zeros[2] + mu)/(-2.5));
+					f_H[i] = f_ab_zero*nu_c[3] * pow(10, (H_out[i] - ab_zeros[3] + mu)/(-2.5));
+					f_F[i] = f_ab_zero*nu_c[4] * pow(10, (F_out[i] - ab_zeros[4] + mu)/(-2.5));
 				}
-			}
-			//cout << "     " << n_needed << " stars are in the field of view" << "\n";
-			//cout << "     " << n_detected << " stars are detected per square degree" << "\n";
-			output << n_detected/(FoV_as/(sqdeg_sqas*d*d)) << " ";
-		} //loop over surface brightness
-		output << "\n";
-	} // loop over distances
 
-	// close output file
-	output.close();
 
+				// cumulative sum of flux array
+				double CS_fJ = 0;
+				int n_needed = 0;
+				while((CS_fJ < f_S_J_tot) && (n_needed < N_imf_filt)){
+					CS_fJ += f_J[n_needed];
+					n_needed++;
+				}
+				// check to make cure that the whole filter wasn't needed
+				if (n_needed==N_imf_filt){
+					cout << "Used the entire IMF. You should decrease the size of the field or increase the IMF sample." << "\n";
+				}
+				else if (n_needed<100){
+					cout << "Needed less than 100 stars to reach required luminosity.\n";
+				}
+
+				// count the number that are above the detection threshold
+				int n_detected = 0;
+				for(i=0;i<n_needed;i++){
+					if(J_out[i] + mu<ps_detect_5slim[2]){
+						n_detected++;
+					}
+				}
+				//cout << "     " << n_needed << " stars are in the field of view" << "\n";
+				//cout << "     " << n_detected << " stars are detected per square degree" << "\n";
+				output << n_detected/(FoV_as/(sqdeg_sqas*d*d)) << " ";
+			} //loop over surface brightness
+			output << "\n";
+		} // loop over distances
+
+		// close output file
+		output.close();
+	} //end of IMF loop
 	// de-allocate memory
 	free(kmass);
 	free(imass);
